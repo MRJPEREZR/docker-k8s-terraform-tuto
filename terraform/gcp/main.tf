209e54c1d10c3e5f7f097f7bc8db7efa3cbc8cdf
jperezr <julian.perez-ramirez@imt-atlantique.net>
2026-03-16 18:33:17 +0100

    fixing ingress

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -2,7 +2,7 @@
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.zone
-  project  = var.project_id 
+  project  = var.project_id
 
   remove_default_node_pool = true
   initial_node_count       = 1
@@ -12,48 +12,53 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = var.node_pool_name
   location   = var.zone
   cluster    = google_container_cluster.primary.name
-  project    = var.project_id 
+  project    = var.project_id
   node_count = var.node_count
 
   node_config {
     preemptible  = true
     machine_type = var.machine_type
 
-    oauth_scopes    = [
+    oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
 }
 
-# data "google_client_config" "default" {}
+data "google_client_config" "default" {}
 
-# resource "kubernetes_manifest" "storages" {
-#   depends_on = [google_container_cluster.primary]
-#   manifest = yamldecode(file("${path.root}/../../k8s/storages.yaml"))
-# }
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.primary.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+}
 
-# resource "kubernetes_manifest" "deployments" {
-#   depends_on = [kubernetes_manifest.storages]
-#   for_each = { for d in yamldecode_all(file("${path.root}/../../k8s/deployments.yaml")) : d.metadata.name => d }
+resource "kubernetes_manifest" "storages" {
+  depends_on = [google_container_cluster.primary]
+  for_each   = { for f in fileset("${path.root}/../../k8s/storages", "*.yaml") : f => f }
+  manifest   = yamldecode(file("${path.root}/../../k8s/storages/${each.value}"))
+}
 
-#   manifest = each.value
-# }
+resource "kubernetes_manifest" "deployments" {
+  depends_on = [kubernetes_manifest.storages]
+  for_each   = { for f in fileset("${path.root}/../../k8s/deployments", "*.yaml") : f => f }
+  manifest   = yamldecode(file("${path.root}/../../k8s/deployments/${each.value}"))
+}
 
-# resource "kubernetes_manifest" "services" {
-#   depends_on = [google_container_cluster.primary]
-#   for_each = { for d in yamldecode_all(file("${path.root}/../../k8s/services.yaml")) : d.metadata.name => d }
+resource "kubernetes_manifest" "services" {
+  depends_on = [google_container_cluster.primary]
+  for_each   = { for f in fileset("${path.root}/../../k8s/services", "*.yaml") : f => f }
+  manifest   = yamldecode(file("${path.root}/../../k8s/services/${each.value}"))
+}
 
-#   manifest = each.value
-# }
+resource "kubernetes_manifest" "ingress" {
+  depends_on = [kubernetes_manifest.services]
+  for_each   = { for f in fileset("${path.root}/../../k8s/ingress", "*.yaml") : f => f }
+  manifest   = yamldecode(file("${path.root}/../../k8s/ingress/${each.value}"))
+}
 
-# resource "kubernetes_manifest" "ingress" {
-#   depends_on = [kubernetes_manifest.services]
-#   manifest = yamldecode(file("${path.root}/../../k8s/ingress.yaml"))
-# }
-
-# resource "kubernetes_manifest" "jobs" {
-#   depends_on = [kubernetes_manifest.deployments, kubernetes_manifest.services]
-#   manifest = yamldecode(file("${path.root}/../../k8s/jobs.yaml"))
-# }
-
-
+resource "kubernetes_manifest" "jobs" {
+  depends_on = [kubernetes_manifest.deployments, kubernetes_manifest.services]
+  for_each   = { for f in fileset("${path.root}/../../k8s/jobs", "*.yaml") : f => f }
+  manifest   = yamldecode(file("${path.root}/../../k8s/jobs/${each.value}"))
+}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,12 +3,3 @@ provider "google" {
   region      = "europe-west3"
   zone        = "europe-west3-a"
 }
-
-provider "kubernetes" {
-  host                   = google_container_cluster.primary.endpoint
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
-}
-
-provider "docker" {
-}
