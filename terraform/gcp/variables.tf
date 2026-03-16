180c5cc7d02d15b3ae062d1e67aae97929bfa0d1
jperezr <julian.perez-ramirez@imt-atlantique.net>
2026-03-16 15:02:14 +0100

    starting terraform gcp part

---
+++ terraform/gcp/variables.tf
@@ -0,0 +1,20 @@
+variable "project_id" {
+  default = "cloud-login-489913"
+}
+variable "region" {
+  default = "europe-west3"
+}
+variable "zone" {
+  default = "europe-west3-a"
+}
+variable "cluster_name" {
+  default = "my-cluster"
+}
+
+variable "machine_type" {
+  default = "n2-standard-2"
+}
+
+variable "node_count" {
+  default = 2
+}
